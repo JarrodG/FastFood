777282bcd37cc92d4b20dc57cff582d61b425e70
JeremySkrdlant <jeremy.skrdlant@nwktc.edu>
2014-09-30 09:31:53 -0600

    Added stub function in DatabaseConnection

--- FastFood/DatabaseConnection.swift
+++ FastFood/DatabaseConnection.swift
@@ -11,4 +11,11 @@ import Foundation
 
 class DataConnection {
     
+    //Used by the collection view. Pass in category and get a string array with [name, url of image]
+    
+    func getCollectionViewData(categoryName:String)->[String]{
+        
+        return []
+    }
+    
 }
